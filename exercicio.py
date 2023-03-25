88a9e8e921b3fcb726ec6a7515867451eda5ffaa
sheila <sheila.taylorbruno@gmail.com>
2023-03-25 20:01:20 -0300

    first commit

---
+++ exercicio.py
@@ -0,0 +1,13 @@
+Nome = "José"
+Sobrenome = 'Martins'
+Idade = 32
+Ano_de_nascimento = 2022 - Idade
+Maior_de_idade = Idade >= 18
+Altura_metros = 1.80
+
+print('Nome:', Nome) 
+print('Sobrenome:', Sobrenome)
+print('Idade:', Idade)
+print('Ano de nascimento:', Ano_de_nascimento)
+print('É maior de idade?', Maior_de_idade)
+print('Altura em metros:', Altura_metros)
